621c109ceb5f70b7a99c0e86cbbe126f6d6012d8
Juan Manuel Suárez <juanmanuelsuarez10@gmail.com>
2022-03-02 09:44:21 -0300

    Add engine flames

--- Assets/Scripts/EngineController.cs
+++ Assets/Scripts/EngineController.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 
 public class EngineController : MonoBehaviour
 {
+    public String fireKey;
+
     public float thrustMagnitude = 100;
     public Rigidbody capsuleRigidbody;
-    public String fireKey;
+    
+    private ParticleSystem flameParticleSystem;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        flameParticleSystem = GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -19,6 +22,8 @@ public class EngineController : MonoBehaviour
         if (Input.GetAxis(fireKey) > 0)
         {
             Fire();
+        } else {
+            Stop();
         }
     }
 
@@ -26,5 +31,14 @@ public class EngineController : MonoBehaviour
     {
         capsuleRigidbody.AddForceAtPosition(transform.up * thrustMagnitude, transform.position, 
                                             ForceMode.Impulse);
+        if (!flameParticleSystem.isPlaying)
+        {
+            flameParticleSystem.Play();
+        }
+    }
+
+    public void Stop()
+    {
+        flameParticleSystem.Stop();
     }
 }
